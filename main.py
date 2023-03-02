21f8640870eac385dc830e6bd0c24fc0ddd4deb5
aclongo <125140476+aclongo@users.noreply.github.com>
2023-03-02 10:12:22 -0500

    Create a main game loop that allows user to replay or quit

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def mad(mood):
         print('Neko simply blinks at you with an air of superiority! Silly human, try again.')
         return mood 
     elif action == 's':
-        print('Neko bit you, ouch! Try again.')
+        print('Neko bit you, ouch! (Mood has decreased)')
         return mood - 1
     elif action == 'l':
         print('Neko is pleased that you respected their boundaries. (Mood has improved.)')
@@ -25,7 +25,7 @@ def mad(mood):
 
 def hungry(mood):
     print('Neko keeps following you around and meowing for... somthing?')
-    print(action)
+    print(action_choice)
     action = input() 
     if action == 'f':
         print('Neko eats all of the food! (Mood has improved)')
@@ -74,33 +74,38 @@ def sleepy(mood):
         print('Neko hides under the bed and naps alone!')
         return mood
 
-print('Neko-Neko : a text-based adventure about taking care of a needy cat')
-print('Can you make Neko happy? All cats are difficult to read, and Neko is no exception.')
-print('Respond to her mood by choosing actions. Be careful, too many incorrect actions may make her run away!')
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-action_choice = 'Choose an action: (f)eed, (p)lay, (s)nuggle, (l)eave alone'
-
 # generate a random number that correlates to different moods
 mood = random.randrange(1, 5)
 
 # check what the current mood value is and pass it to the correct function
 # if the correct action is taken within a fuction, the mood will increase value
 # if mood reaches 5 (happy), the program ends
-while True:
-    if mood == 1:
-        mood = mad(mood)
-    elif mood == 2:
-        mood = hungry(mood)
-    elif mood == 3:
-        mood = bored(mood)
-    elif mood == 4:
-        mood = sleepy(mood)
-    elif mood == 0: # if mood reaches 0, Neko runs awaw and game ends
-        print('Oh no! It looks like Neko ran away...')
-        break
-    else: # if mood reaches 5, Neko is happy and game ends
-        print('Great! You were able to navigate Nekos moods and make her happy.')
-        break
-
-print('Would you like to play again? y/n')
-
+while True: # main game loop
+    print('Neko-Neko : a text-based adventure about taking care of a needy cat')
+    print('Can you make Neko happy? All cats are difficult to read, and Neko is no exception.')
+    print('Respond to her mood by choosing actions. Be careful, too many incorrect actions may make her run away!')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    action_choice = 'Choose an action: (f)eed, (p)lay, (s)nuggle, (l)eave alone'
+    
+    while True:
+        if mood == 1:
+         mood = mad(mood)
+        elif mood == 2:
+            mood = hungry(mood)
+        elif mood == 3:
+            mood = bored(mood)
+        elif mood == 4:
+            mood = sleepy(mood)
+        elif mood == 0: # if mood reaches 0, Neko runs awaw and game ends
+            print('Oh no! It looks like Neko ran away...')
+            break
+        else: # if mood reaches 5, Neko is happy and game ends
+            print('Great! You were able to navigate Nekos moods and make her happy.')
+            break
+    
+    print('Would you like to play again? y/n')
+    replay = input()
+    if replay == 'y':
+        continue
+    if replay == 'n':
+        quit()
