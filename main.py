7aff0a92451e65604037c488db984271da30b03b
aclongo <125140476+aclongo@users.noreply.github.com>
2023-03-02 09:51:19 -0500

    Refine instructions, add variable for action_choice

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import random
 	#incorrect action, return the same mood value to the loop to try again
 	#correct action, mood increases by 1
 def mad(mood):
-    print('Neko is mad.') 
-    print('Enter a lowercase letter: (f)eed, (p)lay, (s)nuggle, (l)eave alone') 
+    print('Neko seems to be easily agitated and aloof right now.') 
+    print(action_choice) 
     action = input() 
     if action == 'f':
         print('Neko walked away with disinterest! Try again.')
@@ -18,14 +18,14 @@ def mad(mood):
         return mood 
     elif action == 's':
         print('Neko bit you, ouch! Try again.')
-        return mood
+        return mood - 1
     elif action == 'l':
         print('Neko is pleased that you respected their boundaries. (Mood has improved.)')
         return mood + 1 
 
 def hungry(mood):
-    print('Neko is hungry.')
-    print('Enter a lowercase letter: (f)eed, (p)lay, (s)nuggle, (l)eave alone')
+    print('Neko keeps following you around and meowing for... somthing?')
+    print(action)
     action = input() 
     if action == 'f':
         print('Neko eats all of the food! (Mood has improved)')
@@ -41,8 +41,8 @@ def hungry(mood):
         return mood 
 
 def bored(mood):
-    print('Neko is bored.')
-    print('Enter a lowercase letter: (f)eed, (p)lay, (s)nuggle, (l)eave alone') 
+    print('Neko seems to have a lot of excess energy right now.')
+    print(action_choice) 
     action = input() 
     if action == 'f':
         print('Neko plays with the food and makes a mess! Try again.')
@@ -58,25 +58,27 @@ def bored(mood):
         return mood
 
 def sleepy(mood): 
-    print('Neko is sleepy.') 
-    print('Enter a lowercase letter: (f)eed, (p)lay, (s)nuggle, (l)eave alone') 
+    print('Neko seems to have low energy right now.') 
+    print(action_choice) 
     action = input()
     if action == 'f':
-        print('Neko sniffs at the food before deciding it would take too much energy to eat now. Try again.')
+        print('Neko sniffs at the food before deciding it would take too much energy to eat now.')
         return mood
     elif action == 'p':
-        print('Neko lazily bats at the toy a few times before ignoring you! Try again.')
+        print('Neko lazily bats at the toy a few times before ignoring you!')
         return mood 
     elif action == 's':
-        print('Neko purrs contentedly and drifts off to sleep in your lap. (Mood has improved.)')
+        print('Neko purrs contentedly and drifts off to sleep in your lap.')
         return mood + 1 
     elif action == 'l':
-        print('Neko hides under the bed and naps alone! Try again.')
+        print('Neko hides under the bed and naps alone!')
         return mood
 
-print('Neko neko! ^_^')
-print('Can you make Neko happy?')
-print('Respond to her mood.')
+print('Neko-Neko : a text-based adventure about taking care of a needy cat')
+print('Can you make Neko happy? All cats are difficult to read, and Neko is no exception.')
+print('Respond to her mood by choosing actions. Be careful, too many incorrect actions may make her run away!')
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+action_choice = 'Choose an action: (f)eed, (p)lay, (s)nuggle, (l)eave alone'
 
 # generate a random number that correlates to different moods
 mood = random.randrange(1, 5)
@@ -93,7 +95,12 @@ while True:
         mood = bored(mood)
     elif mood == 4:
         mood = sleepy(mood)
-    else: # if mood reaches 5, Neko is happy and program ends
+    elif mood == 0: # if mood reaches 0, Neko runs awaw and game ends
+        print('Oh no! It looks like Neko ran away...')
+        break
+    else: # if mood reaches 5, Neko is happy and game ends
+        print('Great! You were able to navigate Nekos moods and make her happy.')
         break
 
-print('Yay! Neko is happy!')
+print('Would you like to play again? y/n')
+
